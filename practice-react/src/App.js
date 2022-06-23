90239b81421e2893589a68d4de9109290cf142f6
Felipe <machadofelipe2016@outlook.com>
2022-06-23 11:58:14 -0300

    c1-practice and create another ways to create list with usestate

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,10 +1,18 @@
+import React,{useState} from 'react'
 
 import './App.css';
 
 function App() {
+  const [list]= useState(['Felipe','Programmer','Programer-floripa'])
+  
   return (
     <div className="App">
       <h1>testing another way of practice and learn more react</h1>
+
+      <ul>
+        {list.map((iten, i) => (<li key={iten} > {i}  </li>))}
+      </ul>
+    
     </div>
   );
 }
