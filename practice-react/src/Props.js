980106eb79a01a5edffde399712991d060d6ad86
Felipe <machadofelipe2016@outlook.com>
2022-06-23 15:00:15 -0300

    c8-create new file to practice basic react

---
+++ practice-react/src/Props.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateAllprops = (props) => {
+    return(<>
+     <div>
+        this is my name  {props.name} and i am here to talk about several threads, how create new features provides less rules ??? 
+     </div>
+    </>)
+}
+
+
+export default CreateAllprops
