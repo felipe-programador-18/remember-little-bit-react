c12aedf44d949a3ecfff0be1c20171d3ac69aee1
Felipe <machadofelipe2016@outlook.com>
2022-06-23 11:31:22 -0300

    c0-started new practice and create new idea of improve code

---
+++ practice-react/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>testing another way of practice and learn more react</h1>
+    </div>
+  );
+}
+
+export default App;
