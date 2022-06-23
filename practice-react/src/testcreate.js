99afbc9b4ba624dbefa80130e97d9b66a32c2410
Felipe <machadofelipe2016@outlook.com>
2022-06-23 17:12:19 -0300

    c11-create file call testecreate

---
+++ practice-react/src/testcreate.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CreateProps = ({children}) => {
+    return (<>
+      <h1>Practice and adding some skills!!!</h1>
+      {children}{children}
+    </>)
+}
+
+export default CreateProps
