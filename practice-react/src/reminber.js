38bde957007c4a6ef51115ddafee5f9cfedfb3b4
Felipe <machadofelipe2016@outlook.com>
2022-06-29 10:09:48 -0300

    c17-adding more code

--- practice-react/src/reminber.js
+++ practice-react/src/reminber.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const CreateAnotherPrac = () => {
   const [name] = useState('Martins')
-
+  const [show, setShow] = useState(true)
   return(<>
-      {name === 'Martins' ? <p>your name is {name}</p> : <p>your name não é martins</p> }
+     
+       {name === 'Martins' ? <p>your name is {name}</p> : <p>your name não é martins</p> }
+       {show && <p> hide this paragraph</p>}
+              
+      <button  onClick={()=> setShow(curr => !curr) } >Show name</button>
   
   </>)
 
