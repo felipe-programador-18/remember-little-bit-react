108a7de5d9fb91ad2e6d0919a65e6d00b9e10244
Felipe <machadofelipe2016@outlook.com>
2022-07-03 16:45:31 -0300

    c18-create another component to adding and practice more about create component with children

---
+++ practice-react/src/CreateChildren.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CreateChildren = ({children, newchildren}) => {
+
+
+   return (<>
+   
+    <h1> adding more children to practice about context!!!{children} </h1>
+    {newchildren}
+   </>)
+}
+
+export default CreateChildren
