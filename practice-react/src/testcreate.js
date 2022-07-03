f7343b67a1068c59e00f678d0ab1520e9e770e11
Felipe <machadofelipe2016@outlook.com>
2022-07-03 16:46:38 -0300

    c19-create new componenet

--- practice-react/src/testcreate.js
+++ practice-react/src/testcreate.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const CreateProps = ({children, newValue}) => {
+const CreateProps = ({children}) => {
     return (<>
       <h1>Practice and adding some skills!!!</h1>
       {children}
-      <p>the value is {newValue} </p>
+      
     </>)
 }
 
